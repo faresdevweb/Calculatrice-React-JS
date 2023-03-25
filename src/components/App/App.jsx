b4a173ec207fa03566e502e775e73a67aa31cf3f
Fares <fares.meier@hotmail.com>
2023-03-25 15:52:51 +0100

    Calculatrice ReactJS terminé
    Premier projet ajouté à github

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,71 @@
-import React from "react";
-import "./App.css"
+import React, { useState } from "react";
+import "./App.css";
+import Screen from "../Screen/Screen";
+import Boutton from "../Boutton/Boutton";
 
 function App() {
-    return (
-        <div className="container">
-            <h1>bonjourlesamis</h1>
-        </div>
-    );
+  const [display, setDisplay] = useState("");
+
+  const specialCharacter = ["+", "-", "/", "*", "."];
+
+  
+  const handleClick = (event) => {
+    const value = event.target.dataset.value;
+    event.preventDefault();
+
+    if (value === "C") {
+      setDisplay("");
+    } else if (value === "=") {
+      const result = eval(display);
+      setDisplay(result);
+    } else if (
+      specialCharacter.includes(value) &&
+      typeof display === "string" &&
+      (specialCharacter.includes(display.slice(-1)) || (value === "." && display.split(/[-+*/]/).slice(-1)[0].includes('.')))
+    ) {
+      return;
+    } else {
+      setDisplay((prevDisplay) => prevDisplay + value);
+    }
+  };
+  
+
+  return (
+    <div className="container">
+      <Screen screenValue={display} />
+      <div className="container-button">
+        {[
+          "0",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          ".",
+          "+",
+          "-",
+          "*",
+          "/",
+          "=",
+          "C"
+        ].map((label, index) => {
+          return (
+            <Boutton
+              key={label}
+              value={label}
+              className={`calculator-button-${index + 1} button`}
+              handleClick={handleClick}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
-import App from './App/App';
-=======
 import App from './components/App/App';
->>>>>>> 878ae85b28a00157d1e77973ec1f555b5606b53e
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,7 +10,3 @@ root.render(
   </React.StrictMode>
 );
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 878ae85b28a00157d1e77973ec1f555b5606b53e
